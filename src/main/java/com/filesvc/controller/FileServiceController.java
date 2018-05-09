47acb2e3304641ffbaa43c23c1dcd1cdfa6e212c
Guruprasad Thorvey <gthorvey@gmail.com>
2018-05-09 18:18:06 +0530

    9th May 6:17 pm commit

--- src/main/java/com/filesvc/controller/FileServiceController.java
+++ src/main/java/com/filesvc/controller/FileServiceController.java
@@ -1,6 +1,7 @@
 package com.filesvc.controller;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -10,6 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,28 +25,10 @@ import com.filesvc.dto.TreeItem;
 
 @RestController
 public class FileServiceController {
-	
-	
-	// TreeNode tn;
-	@CrossOrigin(origins = "http://localhost:4200")
-	@RequestMapping("/getAlbums")
-	public HttpEntity<String> getAlbums() throws Exception {		
-		String resJson = "{\r\n" + 
-				"  \"id\": 1,\r\n" + 
-				"  \"artist\": \"The Prependers\",\r\n" + 
-				"  \"album\": {\r\n" + 
-				"    \"name\": \"Opacity Zero\",\r\n" + 
-				"    \"releaseDate\": \"September 25, 2014\",\r\n" + 
-				"    \"coverImage\": \"assets/opacity-zero.jpg\"\r\n" + 
-				"  }\r\n" + 
-				"}" ;
-		return new ResponseEntity<String>(resJson, HttpStatus.OK);
-	}
 
-	// TreeNode tn;
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/getDirStructure")
-	public HttpEntity<List<TreeItem>> fileContent(@RequestParam(value = "dir") String dir) throws Exception {
+	public HttpEntity<List<TreeItem>> getDirStructure(@RequestParam(value = "dir") String dir) throws Exception {
 		List<TreeItem> treeList = new ArrayList<TreeItem>();
 		File file = new File(dir);
 		if (file != null && file.exists()) {
@@ -53,10 +37,9 @@ public class FileServiceController {
 			rootItem.setExpand(true);
 			String id = "1";
 			rootItem.setID(id);
-			rootItem.setFullPath(file.getAbsolutePath());
-			//rootItem.setFullPath(rootItem.getName());
+			rootItem.setFullPath(StringUtils.replaceChars(file.getAbsolutePath(), '\\', '/'));
 			treeList.add(rootItem);
-			if (file.isDirectory()) {				
+			if (file.isDirectory()) {
 				addChildren(file, id, treeList);
 			}
 		}
@@ -64,7 +47,17 @@ public class FileServiceController {
 	}
 
 	private void addChildren(File file, String parentID, List<TreeItem> treeList) {
-		List<File> childFilesList = Arrays.asList(file.listFiles());
+		List<File> childFilesList = Arrays.asList(file.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				if (name != null && dir != null) {
+					String fileName = name.toLowerCase();
+					return fileName.endsWith(".BMA") || fileName.endsWith(".json")
+							|| (dir.isDirectory() && !fileName.endsWith(".zip"));
+				}
+				return false;
+			}
+		}));
+
 		if (!CollectionUtils.isEmpty(childFilesList)) {
 			int i = 1;
 			for (File childFile : childFilesList) {
@@ -74,8 +67,7 @@ public class FileServiceController {
 					item.setExpand(childFile.isDirectory());
 					item.setID(parentID + "_" + i++);
 					item.setCatagoryId(parentID);
-					item.setFullPath(childFile.getAbsolutePath());
-					//item.setFullPath(childFile.getName());
+					item.setFullPath(StringUtils.replaceChars(childFile.getAbsolutePath(), '\\', '/'));
 					treeList.add(item);
 					if (childFile.isDirectory()) {
 						addChildren(childFile, item.getID(), treeList);
